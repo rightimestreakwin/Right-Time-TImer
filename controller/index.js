f860d4b76014b65366ba67bc078a99ed9724b10f
anand <vermaanand278@gmail.com>
2024-10-17 10:52:52 +0530

    dsffasf

--- controller/index.js
+++ controller/index.js
@@ -6,7 +6,39 @@ const {
 const moment = require("moment");
 const soment = require("moment-timezone");
 const { default: axios } = require("axios");
-
+exports.sendWingoAmountToTheAdmin = async (io) => {
+  try {
+    const data = await queryDb(
+      "SELECT `slot_num`,`game_type`,`round_no`,`mid_amount` FROM wingo_get_amount_on_slot WHERE `game_type` = 1;",
+      []
+    );
+    io.emit("oneminwingoamount", JSON.stringify(data || ""));
+  } catch (e) {
+    console.log(e);
+  }
+};
+exports.sendWingoAmountToTheAdminThreeMin = async (io) => {
+  try {
+    const data = await queryDb(
+      "SELECT `slot_num`,`game_type`,`round_no`,`mid_amount` FROM wingo_get_amount_on_slot WHERE `game_type` = 2;",
+      []
+    );
+    io.emit("oneminwingoamountthreemin", JSON.stringify(data || ""));
+  } catch (e) {
+    console.log(e);
+  }
+};
+exports.sendWingoAmountToTheAdminFiveMin = async (io) => {
+  try {
+    const data = await queryDb(
+      "SELECT `slot_num`,`game_type`,`round_no`,`mid_amount` FROM wingo_get_amount_on_slot WHERE `game_type` = 3;",
+      []
+    );
+    io.emit("oneminwingoamountfivemin", JSON.stringify(data || ""));
+  } catch (e) {
+    console.log(e);
+  }
+};
 exports.generatedTimeEveryAfterEveryOneMin = (io) => {
   const job = schedule.schedule("* * * * * *", function () {
     const currentTime = new Date();
@@ -78,7 +110,6 @@ exports.generatedTimeEveryAfterEveryOneMinTRX = (io) => {
             );
           });
       }, [4000]);
-    
     }
   });
 };
@@ -106,4 +137,81 @@ const sendOneMinResultToDatabase = async (time, obj) => {
     .catch((e) => {
       console.log(e);
     });
+};
+
+//////// gateway///////////
+// exports.generatedTimeEveryAfterEveryOneMinbyCrown = () => {
+//   schedule.schedule("*/5 * * * *", function () {
+//     INRWITHDRAWALCALLBACK();
+//     console.log("function is called now");
+//   });
+// };
+exports.INRWITHDRAWALCALLBACK = async () => {
+  try {
+    const get_all_pending_record =
+      "SELECT * FROM `tr15_fund_request` WHERE `admin_approval` = 1 AND `tr15_status` = 1;";
+    const data = await queryDb(get_all_pending_record, []);
+    data.forEach(async (element) => {
+      await CAllbackAPIunction(element);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const CAllbackAPIunction = async (tableData) => {
+  try {
+    const fd = new FormData();
+    fd.append("UserID", "7985913351");
+    fd.append("Token", "32543253535796018096423746274583570018");
+    fd.append("txttransid", tableData?.tr15_trans);
+
+    const result = await axios.post(
+      "https://indian.vpayout.com/Callback_payout/check_transaction_status_withdrwal",
+      fd
+    );
+    if (result?.data?.status === "SUCCESS") {
+      const save_data =
+        "UPDATE `tr15_fund_request` SET admin_approval = 1,`tr15_status` = 2,success_date = NOW(),tr15_response = ?,callback_response = ? WHERE `tr15_id` = ? AND `tr15_trans` = ? AND `tr15_status` = 1;";
+      await queryDb(save_data, [
+        JSON.stringify(result?.data || ""),
+        JSON.stringify(result?.data || ""),
+        Number(tableData?.tr15_id),
+        String(tableData?.tr15_trans),
+      ]);
+    } else if (result?.data?.status === "FAILED") {
+      const save_data =
+        "UPDATE `tr15_fund_request` SET admin_approval = 1,`tr15_status` = 3,success_date = NOW(),tr15_response = ?,callback_response=? WHERE `tr15_id` = ? AND `tr15_trans` = ? AND `tr15_status` = 1;";
+      await queryDb(save_data, [
+        JSON.stringify(result?.data || ""),
+        JSON.stringify(result?.data || ""),
+        Number(tableData?.tr15_id),
+        String(tableData?.tr15_trans),
+      ]);
+      const callback_leser =
+        "INSERT INTO leser(`l01_user_id`,`l01_type`,`l01_transection_type`,`l01_amount`) VALUES(?,?,?,?);";
+      await queryDb(callback_leser, [
+        Number(tableData?.userid),
+        "Caseback",
+        `Withdrawal Cashback get successfully. From TransId: ${tableData?.tr15_trans}`,
+        tableData?.tr15_amt,
+      ]);
+      const callback_user =
+        "UPDATE `user` SET `winning_wallet` = IFNULL(`winning_wallet`,0) + ? WHERE `id` = ?;";
+      await queryDb(callback_user, [
+        Number(tableData?.tr15_amt),
+        Number(tableData?.userid),
+      ]);
+    } else {
+      const save_data =
+        "UPDATE `tr15_fund_request` SET admin_approval = 1,`tr15_status` = 1,tr15_response = ? WHERE `tr15_id` = ? AND `tr15_trans` = ? AND `tr15_status` = 1;";
+      await queryDb(save_data, [
+        JSON.stringify(result?.data || ""),
+        Number(tableData?.tr15_id),
+        String(tableData?.tr15_trans),
+      ]);
+    }
+  } catch (e) {
+    console.log(e);
+  }
 };
--- index.js
+++ index.js
@@ -50,6 +50,21 @@ if (x) {
 }
 ///////////////
 aviator_Start_function(io);
+// allroutes.generatedTimeEveryAfterEveryOneMinbyCrown();
+
+setInterval(() => {
+  allroutes.INRWITHDRAWALCALLBACK();
+}, 5*60*1000);
+setInterval(() => {
+  allroutes.sendWingoAmountToTheAdmin(io);
+}, 5000);
+setInterval(() => {
+  allroutes.sendWingoAmountToTheAdminThreeMin(io);
+}, 10000);
+setInterval(() => {
+  allroutes.sendWingoAmountToTheAdminFiveMin(io);
+}, 1000);
+
 app.get("/", (req, res) => {
   res.status(200).json({
     msg: "Server is running on port 2343",
