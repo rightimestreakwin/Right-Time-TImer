ac1f7f12e589e24cd3c9393e8a696165833be968
anand <vermaanand278@gmail.com>
2024-10-22 13:35:52 +0530

    asf

--- controller/index.js
+++ controller/index.js
@@ -41,12 +41,16 @@ exports.sendWingoAmountToTheAdminFiveMin = async (io) => {
 };
 exports.generatedTimeEveryAfterEveryOneMin = (io) => {
   const job = schedule.schedule("* * * * * *", function () {
-    const currentTime = new Date();
-    const timeToSend =
-      currentTime.getSeconds() > 0
-        ? 60 - currentTime.getSeconds()
-        : currentTime.getSeconds();
-    io.emit("onemin", timeToSend);
+    const now = new Date();
+    const nowIST = soment(now).tz("Asia/Kolkata");
+
+    const currentMinute = nowIST.minutes();
+    const currentSecond = nowIST.seconds();
+    // const timeToSend =
+    //   currentTime.getSeconds() > 0
+    //     ? 60 - currentTime.getSeconds()
+    //     : currentTime.getSeconds();
+    io.emit("onemin", `${currentMinute}_${currentSecond}`);
   });
 };
 
